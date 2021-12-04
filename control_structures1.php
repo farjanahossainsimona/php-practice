a5f9610d2dbc9fa990ca3b55db6b1a6fe806e389
farjana <farjana.96.hossain@gmail.com>
2021-12-04 18:53:01 +0600

    control_structures:if else 1

---
+++ control_structures1.php
@@ -0,0 +1,13 @@
+<?php
+$my_age = 27;
+$your_age = 27;
+if($your_age > $my_age){
+    echo "you older than me";
+
+}else if($your_age < $my_age  ){
+    echo "you younger than me";
+
+}else{
+    echo "we are both similer Aged";
+}
+?>
