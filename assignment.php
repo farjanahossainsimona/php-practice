b3f67178f0304b850bdc847590f56909b12c7db1
farjana <farjana.96.hossain@gmail.com>
2021-12-04 21:41:51 +0600

    assignment operators

---
+++ assignment.php
@@ -0,0 +1,37 @@
+<?php
+
+//addition
+$a = 5;
+// $a = $a+5;
+$a +=10;
+echo $a;
+echo "<br>";
+
+
+//subtraction
+$b = 4;
+$b -=2;
+echo $b;
+echo "<br>";
+
+//multiplication
+$c = 3;
+$c *=3;
+echo $c;
+echo "<br>";
+
+
+
+//division
+$d = 20;
+$d /=2;
+echo $d;
+echo "<br>";
+
+//modulus
+$e = 10;
+$e %=3;
+echo $e;
+
+
+?>
