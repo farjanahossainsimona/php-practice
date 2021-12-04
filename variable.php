b85338f3dae8eef4490681ab79eab57ed5a9d4b5
farjana <farjana.96.hossain@gmail.com>
2021-12-04 18:04:35 +0600

    variable

---
+++ variable.php
@@ -0,0 +1,20 @@
+<?php
+$message = "Hello small";
+$Message = "hello capital";
+$messAge = "hello all";
+echo $message;
+echo "<br>";
+echo $Message;
+echo "<br>";
+echo $messAge;
+//data type
+$one = "data";//string
+$two = 123;//Integer
+$three = 12.1;//Float
+$four = true;//boolean
+$five = array(123,'simona');//Array
+$six = Null;//Null
+echo "<br>";
+//to check data type
+echo var_dump($five);
+?>
