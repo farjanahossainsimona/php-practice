4f3a1442331374dc405ef99bbc3882854f01a2bd
farjana <farjana.96.hossain@gmail.com>
2021-12-06 11:47:38 +0600

    increment decrement

---
+++ increment_decrement.php
@@ -0,0 +1,13 @@
+<?php
+$a = 20;
+$b = 10;
+// $a = $a +1;//post
+// $a = 1 + $a
+$a++;//post increment
+// ++$a;//pre increment
+$b--;//post decrement
+// --$b;//pre increment
+echo $a;
+echo "<br>";
+echo $b;
+?>
