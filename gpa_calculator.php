5f867fa045ed3fcb0f3a554b5713603e5479cdc2
farjana <farjana.96.hossain@gmail.com>
2021-12-19 10:34:35 +0600

    gpa calculator using logical operator

---
+++ gpa_calculator.php
@@ -0,0 +1,22 @@
+<?php
+$marks = 69;
+if($marks >= 80 && $marks <=100){
+    echo"You got A+";
+
+}elseif($marks >= 80 && $marks <=100){
+    echo"You got A";
+}elseif($marks >= 70 && $marks <=79){
+    echo"You got A";
+}elseif($marks >= 60 && $marks <=69){
+    echo"You got A";
+}elseif($marks >= 50 && $marks <=59){
+    echo"You got A";
+}elseif($marks >= 40 && $marks <=49){
+    echo"You got A";
+}elseif($marks >= 33 && $marks <=39){
+    echo"You got A";
+}else{
+    echo"Invalid Number";  
+}
+
+?>
